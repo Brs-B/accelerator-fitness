d5f92ab3b4ecdd99ade6d86124994a2df7607148
Boris Bakhtin <borisbakhtin@yandex.ru>
2023-05-31 08:57:47 +0300

    Создание блока отзывов

--- source/js/utils/slider.js
+++ source/js/utils/slider.js
@@ -1,36 +1,44 @@
+// // eslint-disable-next-line no-undef
+// const swiper = new Swiper('.mySwiper', {
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//       initialSlide: 2,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 30,
+//       initialSlide: 2,
+//     },
+//     1000: {
+//       slidesPerView: 3,
+//       spaceBetween: 40,
+//       initialSlide: 2,
+//     },
+//     1300: {
+//       slidesPerView: 'auto',
+//       spaceBetween: 40,
+//       initialSlide: 0,
+//     },
+//   },
+//   loop: true,
+//   navigation: {
+//     nextEl: '.coaches__arrow--next',
+//     prevEl: '.coaches__arrow--prev',
+//   },
+// });
+
+// swiper();
+
 // eslint-disable-next-line no-undef
-const swiper = new Swiper('.swiper', {
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      initialSlide: 2,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      initialSlide: 2,
-    },
-    1000: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-      initialSlide: 2,
-    },
-    1300: {
-      slidesPerView: 'auto',
-      spaceBetween: 40,
-      initialSlide: 0,
-    },
-  },
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+const newSwiper = new Swiper('.newSwiper', {
+  slidesPerView: 1,
+  loop: false,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.reviews__arrow--next',
+    prevEl: '.reviews__arrow--prev',
   },
 });
 
-swiper();
+newSwiper();
