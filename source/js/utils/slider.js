9bc840ac637c447f49f3cc0c3f5eb570ad844c23
Boris Bakhtin <borisbakhtin@yandex.ru>
2023-05-31 12:54:45 +0300

    Создание остальных блоков и некоторые фиксы

--- source/js/utils/slider.js
+++ source/js/utils/slider.js
@@ -1,35 +1,35 @@
-// // eslint-disable-next-line no-undef
-// const swiper = new Swiper('.mySwiper', {
-//   breakpoints: {
-//     320: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//       initialSlide: 2,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 30,
-//       initialSlide: 2,
-//     },
-//     1000: {
-//       slidesPerView: 3,
-//       spaceBetween: 40,
-//       initialSlide: 2,
-//     },
-//     1300: {
-//       slidesPerView: 'auto',
-//       spaceBetween: 40,
-//       initialSlide: 0,
-//     },
-//   },
-//   loop: true,
-//   navigation: {
-//     nextEl: '.coaches__arrow--next',
-//     prevEl: '.coaches__arrow--prev',
-//   },
-// });
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.mySwiper', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      initialSlide: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      initialSlide: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      initialSlide: 2,
+    },
+    1300: {
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      initialSlide: 0,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.coaches__arrow--next',
+    prevEl: '.coaches__arrow--prev',
+  },
+});
 
-// swiper();
+swiper();
 
 // eslint-disable-next-line no-undef
 const newSwiper = new Swiper('.newSwiper', {
